f00129171e5a299f0eeac5cd24ddd3825ded5522
Mick Hansen <doomschild@gmail.com>
2013-03-25 16:28:55 +0100

    Handle some issues with undefiend values and setting

--- dottie.js
+++ dottie.js
@@ -1,8 +1,8 @@
 (function(undefined) {
 	var root = this;
-	
+
 	// Object cloning function, uses jQuery/Underscore/Object.create depending on what's available
-	
+
 	var clone = function (object) {
 		if (typeof Object.create !== 'undefined') {
 			return Object.create(object);
@@ -13,21 +13,21 @@
 		if (typeof _ !== 'undefined') {
 			return _.extend({}, object);
 		}
-		
+
 	};
-	
+
 	// Weird IE shit, objects do not have hasOwn, but the prototype does...
 	var hasOwnProp = Object.prototype.hasOwnProperty;
 
 	var Dot = function() {
 		var args = Array.prototype.slice.call(arguments);
-		
+
 		if (args.length == 2) {
 			return Dot.find.apply(this, args);
 		}
 		return Dot.transform.apply(this, args);
 	};
-	
+
 	// Legacy syntax, changed syntax to have get/set be similar in arg order
 	Dot.find = function(path, object) {
 		return Dot.get(object, path);
@@ -36,7 +36,7 @@
 	// Traverse object according to path, return value if found - Return undefined if destination is unreachable
 	Dot.get = function(object, path) {
 		var pieces = path.split('.'), current = object, piece;
-		
+
 		if (current) {
 			for (var index in pieces) {
 				piece = pieces[index];
@@ -57,12 +57,14 @@
 	// Set nested value
 	Dot.set = function(object, path, value) {
 		var pieces = path.split('.'), current = object, piece, length = pieces.length;
-		
+
 		for (var index in pieces) {
 			piece = pieces[index];
-			if (!hasOwnProp.call(current, piece)) {
+			if (!hasOwnProp.call(current, piece) || current[piece] === undefined) {
 				current[piece] = {};
 			}
+
+			if (typeof current !== 'object') throw new Error('Target key is not suitable for a nested value (it is either not undefined or not an object)');
 
 			if (index == (length - 1)) {
 				current[piece] = value;
@@ -80,7 +82,7 @@
 			Dot.set(object, path, value);
 		}
 	};
-	
+
 	// Transform unnested object with .-seperated keys into a nested object.
 	Dot.transform = function(object) {
 		var pieces, piecesLength, current, transformed = clone(object), piece;
@@ -89,13 +91,13 @@
 				pieces = key.split('.');
 				piecesLength = pieces.length;
 				current = transformed;
-				
+
 				for (var index in pieces) {
 					piece = pieces[index];
 					if (index != (piecesLength - 1) && !current.hasOwnProperty(piece)) {
 						current[piece] = {};
 					}
-					
+
 					if (index == (piecesLength - 1)) {
 						current[piece] = object[key];
 						delete transformed[key];
@@ -106,15 +108,15 @@
 				transformed[key] = transformed[key]; // Ensure that properties exist on the object, not just the prototype
 			}
 		}
-		
+
 		return transformed;
 	};
-	
+
 	if (typeof module !== 'undefined' && module.exports) {
 		exports = module.exports = Dot;
 	} else {
 		root['Dot'] = Dot;
-		
+
 		if (typeof define === "function") {
 			define([], function () { return Dot; });
 		}
--- test/set.test.js
+++ test/set.test.js
@@ -20,4 +20,19 @@ describe("dottie.set", function () {
 		expect(data.level1.level2).toEqual('foo');
 		expect(typeof data.level1).toEqual('object');
 	});
+
+	it("should handle setting a nested value on an undefined value (should convert undefined to object)", function () {
+		var data = {
+			'values': undefined
+		};
+
+		dottie.set(data, 'values.level1', 'foo');
+		expect(data.values.level1).toEqual('foo');
+	});
+
+	it("should throw error when setting a nested value on an existing key with a non-object value", function() {
+		expect(function () {
+			dottie.set(data, 'foo.bar.baz', 'someValue');
+		}).toThrow();
+	});
 });
